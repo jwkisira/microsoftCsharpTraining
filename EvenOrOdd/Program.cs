6c58a2f53e955e3cc84f5a4538a966a572001607
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 02:29:34 +0300

    check whether numbers in array are even or odd

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        // program that checks if numbers in it are even or odd
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for(int i = 0; i < numbers.Length; i++){
+            if (numbers[i] % 2 == 0){
+                Console.WriteLine(numbers[i]+ " is even");
+            }
+            else{
+                Console.WriteLine(numbers[i] + " is odd");
+            }
+        }
+    }
+}
