092792dea3ef50d3146bb7339d00a9a62672f6f6
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 02:45:25 +0300

    Determine what to wear based on the weather outside

---
+++ WeatherForecast/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        // program that evaluates weather conditions using switch statements and returns what clothes to wear
+        string[] weatherForecasts = {"Sunny and warm", "rainy with thunderstorms", "cloudy and cold"};
+
+        for (int i = 0; i < weatherForecasts.Length; i++){
+            switch (weatherForecasts[i]){
+            case "Sunny and warm":
+                Console.WriteLine("The weather is "+ weatherForecasts[0]+ " please dress warm");
+                break;
+            case "rainy with thunderstorms":
+                Console.WriteLine("The weather is "+ weatherForecasts[1]+ " carry an umbrella");
+                break;
+            case "cloudy and cold":
+                Console.WriteLine("The weather is "+ weatherForecasts[2]+ " please carry a sweater");
+                break;
+            default:
+            
+                break;
+        }
+        }
+
+        
+    }
+}
