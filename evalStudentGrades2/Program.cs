a365f358dd79d62ebc154a0029a2425f275921d8
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 21:19:13 +0300

    Evaluate students grades and assign them letter grades

---
+++ evalStudentGrades2/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        // program that uses for and switch to assign letter grades based on score
+        int[] studentGrades = {18, 93, 82, 44, 68, 72, 86};
+
+        for (int i = 0; i < studentGrades.Length; i++){
+            switch (studentGrades[i]){
+                case int n when(studentGrades[i] >= 90):
+                    Console.WriteLine(studentGrades[i] + " - Grade A: GREAT JOB");
+                    break;
+                case int n when(studentGrades[i] >= 80):
+                    Console.WriteLine(studentGrades[i] + " - Grade B: GOOD JOB");
+                    break;
+                case int n when(studentGrades[i] >= 70):
+                    Console.WriteLine(studentGrades[i] + " - Grade C: FAIR JOB");
+                    break;
+                case int n when(studentGrades[i] >= 60):
+                    Console.WriteLine(studentGrades[i] + " - Grade D: ROOM FOR IMPROVEMENT AHH JOB");
+                    break;
+                default:
+                    Console.WriteLine("F - Failed in so much that we cannot show you the grade... pull up your socks");
+                    break;
+            }
+        } 
+    }
+}
