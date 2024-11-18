e4c405fefc22f828f36adfa039d58e07bdd60674
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 11:00:48 +0300

    determine if a student's grade is above or below the passmark (50)

---
+++ PassOrFail/Program.cs
@@ -0,0 +1,16 @@
+﻿// determine if a student's scores pass or fail
+
+class Program{
+    public static void Main(string[] args){
+        int[] studentScores = {45, 60, 72, 38, 55};
+
+        for(int i = 0; i < studentScores.Length; i++){
+            if (studentScores[i] > 50){
+                Console.WriteLine(studentScores[i] + " - Pass");
+            }
+            else{
+                Console.WriteLine(studentScores[i] + " - Fail");
+            }
+        }
+    }
+}
