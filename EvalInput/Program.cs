1e877fac0ebedd71904cfccae4edfc84a19b9f4a
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 20:57:46 +0300

    take user input and ensure it's even

---
+++ EvalInput/Program.cs
@@ -0,0 +1,16 @@
+﻿// program that asks users for input, number between 1 and 10 that is even
+
+int input;
+Console.WriteLine("Enter a number between 1 and 10:");
+input = int.Parse(Console.ReadLine());
+
+do {
+    if (input > 0 && input <= 10 && input%2 == 0){
+        Console.WriteLine("success: "+ input+ " is and even number");
+        break;
+    }
+    else{
+        Console.WriteLine("Invalid input, please try again");
+        break;
+    }
+}while(true);
