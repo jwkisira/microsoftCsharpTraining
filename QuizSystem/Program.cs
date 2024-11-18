36f30e1c29cf5c4754fd6141b5483912609a545e
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 10:40:07 +0300

    calculate total of integers in an array

---
+++ QuizSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+class Program{
+    public static void Main(string[] args){
+        int[] scores = {85, 90, 78, 92, 88};
+
+        int scoresTotal = 0;
+        // loop to iterate over each element of the array
+        for (int i = 0; i<scores.Length; i++){
+            // statement to calclulate the total score
+            scoresTotal = scoresTotal + scores[i];
+        }
+        Console.WriteLine(scoresTotal);
+    }
+}
