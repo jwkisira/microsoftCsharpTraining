cedbcdf2b0f09faf6067dc34478e1d88369ffd4c
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 21:02:44 +0300

    Evaluate if student grades are passes or fails

---
+++ EvalGrades/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program{
+    public static void Main(){
+        // program that evaluates a list of student grades and determines if they passed or failed
+        int[] grades = {82, 45, 66, 81, 89};
+
+        for (int i = 0; i < grades.Length; i++){
+            if (grades[i] >= 65){
+                Console.WriteLine(grades[i]+" - pass");
+            }
+            else{
+                Console.WriteLine(grades[i]+ " - fail");
+            }
+        }
+    }
+}
