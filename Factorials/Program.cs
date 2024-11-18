b9cc53ef133402181a0a68a4b1137460af436a6c
Jesse Kisira <jwkisira@gmail.com>
2024-11-18 10:55:59 +0300

    Calculate factorial of 5

---
+++ Factorials/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+    public static void Main(string[] args){
+        int number = 5;
+
+        int factorial = 0;
+
+        while (number > 1){
+            factorial = number * number - 1;
+            number--;
+        }
+        Console.WriteLine(factorial);
+    }
+}
