9d4c4d5967f80df913f41be4c64cf643310676b1
Jesse Kisira <jwkisira@gmail.com>
2024-11-19 21:38:05 +0300

    Calculate area of provided length and width

---
+++ CalculateArea/Program.cs
@@ -0,0 +1,24 @@
+﻿// program that calculates area of a rectangle based on the input of the user
+
+using System.ComponentModel.DataAnnotations;
+using System.Formats.Asn1;
+
+public class Program{
+    // function to handle calculations
+    public static void calculateArea(int length, int width){
+        Console.WriteLine("length: ");
+        length = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("width: ");
+        width = int.Parse(Console.ReadLine());
+
+        int area = length * width;
+
+        Console.WriteLine(area);
+        // return;
+    }
+
+    public static void Main(string[] args){
+        calculateArea(10, 5);
+    }
+}
