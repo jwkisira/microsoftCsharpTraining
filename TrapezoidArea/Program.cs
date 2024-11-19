c54a2296bc9931ff6b9a7080d21ca08aca4c8d89
Jesse Kisira <jwkisira@gmail.com>
2024-11-19 22:20:51 +0300

    calculate area of trapezoid

---
+++ TrapezoidArea/Program.cs
@@ -0,0 +1,24 @@
+﻿// program to calculate area of trapezoid
+
+public class TrapezoidArea{
+    public static double areaOfTrapezoid(double s1, double s2, double height){
+        return (s1 + s2) / 2 * height;
+    }
+
+    public static void Main(){
+        // prompt user for side 1
+        Console.WriteLine("Enter length of one side:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        // prompt user for side 2
+        Console.WriteLine("Enter length of side 2:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        // prompt user for height
+        Console.WriteLine("Enter the height:");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        // funciton call
+        Console.WriteLine("area of trapezoid = "+ areaOfTrapezoid(side1, side2, height));
+    }
+}
