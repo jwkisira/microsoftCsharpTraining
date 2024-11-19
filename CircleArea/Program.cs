e0a6a9246ac5fd4770892d113c0d367013e408ba
Jesse Kisira <jwkisira@gmail.com>
2024-11-19 22:15:12 +0300

    Calculate area of circle

---
+++ CircleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿// program to calculate area of circle pie*r^2
+
+using System.Globalization;
+using System.Net.NetworkInformation;
+
+public class Program{
+    public static double circleArea(double radius){
+        double pi = Math.PI;
+        radius = Convert.ToDouble(Console.ReadLine());
+        return pi * radius * radius;
+    }
+
+    public static void Main(){
+        // prompt user for radius
+        Console.WriteLine("Enter your radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        // call area function
+        Console.WriteLine("area of your circle is : " + circleArea(radius));
+    }
+}
